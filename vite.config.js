9c712cad29f777758d721e093df1fa4108784d50
Ben Bradford <bradfordium@gmail.com>
2026-05-21 12:43:42 -0500

    update build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-	plugins: [tailwindcss(), svelte()]
-});
+export default defineConfig(({ command }) => ({
+	plugins: [tailwindcss(), svelte()],
+	base: command === 'build' ? '/whats-on-your-seed/' : '/'
+}));
